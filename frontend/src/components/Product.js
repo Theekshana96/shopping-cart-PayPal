81f61d8cb2b2361d8a54057cfe8024d9724017d2
LAPTOP-LCL1VDNQ\user <theekshanajayarathne@gmail.com>
2022-05-25 23:32:52 +0530

    MERN e-commerce shopping cart with paypal sandbox

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+
+const Product = ({ product }) => {
+  return (
+    <Card className='shadow p-3 mb-5 bg-white rounded'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
